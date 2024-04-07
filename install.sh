2406778f2bb50e71b2cfb8a76a1b06852bfb8420
Civan Yavuzsen <civanyavuzsen@gmail.com>
2024-04-07 19:10:50 +0200

    initial commit

---
+++ install.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+install() {
+    local working_dir
+    working_dir=$(pwd)
+
+    echo "Insert the Cloudflare Zone ID:"
+    cf_zone_id=$(systemd-ask-password -n | systemd-creds encrypt --name=cf_zone_id -p - - )
+
+    echo "Insert the Cloudflare Record ID:"
+    cf_record_id=$(systemd-ask-password -n | systemd-creds encrypt --name=cf_record_id -p - - )
+
+    echo "Insert the Auth Email:"
+    cf_auth_email=$(systemd-ask-password -n | systemd-creds encrypt --name=cf_auth_email -p - - )
+
+    echo "Insert the Dns Token:"
+    cf_dns_token=$(systemd-ask-password -n | systemd-creds encrypt --name=cf_dns_token -p - - )
+
+    echo "Insert the Domain:"
+    cf_domain=$(systemd-ask-password -n | systemd-creds encrypt --name=cf_domain -p - - )
+
+    sed 's,!working_dir!,'"$working_dir"',g' update-cloudflare-dns.service |
+    sed 's,!cf_zone_id!,'"$cf_zone_id"',g' |
+    sed 's,!cf_record_id!,'"$cf_record_id"',g' |
+    sed 's,!cf_auth_email!,'"$cf_auth_email"',g' |
+    sed 's,!cf_dns_token!,'"$cf_dns_token"',g' |
+    sed 's,!cf_domain!,'"$cf_domain"',g' > \
+    /etc/systemd/user/update-cloudflare-dns.service
+
+    cp update-cloudflare-dns.timer /etc/systemd/user/.
+
+    systemctl daemon-reload
+    systemctl start update-cloudflare-dns.service
+}
+
+install
---
+++ update-cloudflare-dns.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+update_my_ip() {
+  local now
+  now=$(date -Iseconds)
+
+  local current_ip
+  current_ip=$(curl -s https://api.ipify.org)
+
+  local file_name
+  file_name=$(basename "$0")
+
+  local log_file
+  log_file="${UCF_PWD}/${file_name%.*}".log
+
+  local last_ip
+  if [[ -f "$log_file" ]]; then
+    last_ip=$(awk 'END{print $1}' "$log_file")
+  else
+    touch "$log_file"
+  fi
+
+  if [[ "$current_ip" != "$last_ip" ]]; then
+      echo "updating dns ip with $current_ip"
+      curl -s --request PUT \
+      --url "https://api.cloudflare.com/client/v4/zones/$(cat "$UCF_ZONE_ID")/dns_records/$(cat "$UCF_RECORD_ID")" \
+      --header 'Content-Type: application/json' \
+      --header "X-Auth-Email: $(cat "$UCF_AUTH_EMAIL")" \
+      --header "Authorization: Bearer $(cat "$UCF_DNS_TOKEN")" \
+      --data "$(printf '{
+        "content": "%s",
+        "name": "%s",
+        "proxied": true,
+        "type": "A",
+        "comment": "Domain verification record",
+        "ttl": 300
+      }' "$current_ip" "$(cat "$UCF_DOMAIN")")"
+
+    echo "$current_ip" "$now" >> "$log_file"
+  else
+    echo "same ip"
+  fi
+}
+
+set -e
+update_my_ip
